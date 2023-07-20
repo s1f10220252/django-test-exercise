94a94775284999f71fb12884495b68df82e46ecf
1F10220104 <s1f102201043@iniad.org>
2023-07-20 13:53:23 +0900

    メモ欄を追加してマイグレーション

--- todo/models.py
+++ todo/models.py
@@ -9,6 +9,7 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     posted_at = models.DateTimeField(default=timezone.now)
     due_at = models.DateTimeField(null=True, blank=True)
+    memo = models.CharField(max_length=200, blank=True, null=True)
 
     def is_overdue(self, dt):
         if self.due_at is None:
