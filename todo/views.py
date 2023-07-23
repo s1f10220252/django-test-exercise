b33af83ac3b6c0446eff355024c45fcf8bfd52bc
1F10220244 <s1F102202448@iniad.org>
2023-07-23 21:08:25 +0900

    restart機能を追加

--- todo/views.py
+++ todo/views.py
@@ -79,3 +79,12 @@ def close(request, task_id):
     task.completed = True
     task.save()
     return redirect(index)
+
+def restart(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    task.completed = False
+    task.save()
+    return redirect(index)
